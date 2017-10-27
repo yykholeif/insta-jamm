be225b18040580ea26fabdd4dd7be749232e0c08
Youssef Kholeif <yykholeif@gmail.com>
2017-10-27 00:01:48 -0700

    debugging

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,20 +1,19 @@
 get '/musicians/new' do
-  @instruments = Instrument.all
-  erb :"musicians/new"
+  erb :'musicians/new'
 end
 
 post '/musicians' do
   @musician = Musician.new(params[:musician])
-  if @musician.save
-    login(@musician)
-    redirect "musicians/show/#{@musician.id}"
-  else
-    @errors = @musician.errors.full_messages
-    erb :'musicians/new'
-  end
+    if @musician.save
+      session[:id] = @musician.id
+      redirect "/musicians/#{@musician.id}"
+    else
+      @errors = @musician.errors.full_messages
+      erb :'musicians/new'
+    end
 end
 
-get '/musicians/show/:id' do
+get '/musicians/:id' do
   @musician = Musician.find(params[:id])
   erb :'musicians/show'
 end
